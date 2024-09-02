ffc20bd10211a231e436a1fd51a12ef287b5eb90
ThangNguyen312v <tatthang112113@gmail.com>
2024-09-02 20:47:04 +0700

    baitap

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace democlass{
+    class Program{
+        static void Main(string[] Args){
+            Console.WriteLine("Enter the width:");
+            double width = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the height:");
+            double height = Double.Parse(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle(width, height);
+
+            Console.WriteLine("Your Rectangle \n" + rectangle.Display());
+            Console.WriteLine("Perimeter of the Rectangle: " + rectangle.GetPerimeter());
+            Console.WriteLine("Area of the Rectangle: " + rectangle.GetArea());
+        }
+    }
+}
